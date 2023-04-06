e7b9bd00af84758a35df66c608fbe11b1d61b4d6
Kyle Little <klittle@iteamnm.com>
2023-04-06 13:39:54 -0600

    day 6 update

--- 2022/Day6/csharp/code/Program.cs
+++ 2022/Day6/csharp/code/Program.cs
@@ -1,2 +1,47 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿public class Program
+{
+  public static void Main(string[] args)
+  {
+    string input = File.ReadAllText("D:\\Programming\\repos\\aventOfCode\\code\\code\\code\\input.txt");
+    List<string> fourSet = new List<string>();
+    int targetNumber = 0;
+    int index = 0;
+
+    do
+    {
+      foreach (char letter in input)
+      {
+        string convertedLetter = "" + letter;
+        fourSet.Add(convertedLetter);
+
+        if (fourSet.Count > 14)
+        {
+          fourSet.RemoveAt(0);
+        }
+
+        index++;
+
+        bool check = CheckFourSet(fourSet);
+
+        if (check)
+        {
+          targetNumber = index;
+          break;
+        }
+      }
+    } while (targetNumber == 0 && index < input.Length);
+
+    Console.WriteLine(targetNumber);
+    Console.ReadLine();
+  }
+
+  public static bool CheckFourSet(List<string> _fourSet)
+  {
+    if (_fourSet.Count != 14)
+    {
+      return false;
+    }
+
+    return _fourSet.Distinct().Count() == 14;
+  }
+}
