767b9a9a3b02da420b28d76721947a52c6be68eb
Kyle Little <klittle@iteamnm.com>
2023-04-06 13:39:18 -0600

    day 8 progress

--- 2022/Day8/csharp/trees/Program.cs
+++ 2022/Day8/csharp/trees/Program.cs
@@ -1,2 +1,34 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Globalization;
+using System.Reflection.Metadata.Ecma335;
+
+public class Program
+{
+  public static void Main(string[] args)
+  {
+    string[] input = File.ReadAllLines("D:\\Programming\\repos\\aventOfCode\\trees\\trees\\input.txt");
+
+    int[][] xindex = new int[99][];
+
+    foreach (string line in input)
+    {
+      var splitLines = line.Split();
+
+      for (int i = 0; i < input.Length; i++)
+      {
+        xindex[i] = new int[100];
+
+        foreach (string number in splitLines)
+        {
+          xindex[i][i] = int.Parse(number);
+        }
+      }
+    }
+
+
+    Console.WriteLine();
+    Console.ReadLine();
+
+
+
+  }
+}
