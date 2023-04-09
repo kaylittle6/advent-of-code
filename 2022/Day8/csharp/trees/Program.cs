2ddb1b30c9caf64e26d45f9ac13fb62c7989da95
Kyle Little <klittle@iteamnm.com>
2023-04-09 13:11:38 -0600

    part two complete

--- 2022/Day8/csharp/trees/Program.cs
+++ 2022/Day8/csharp/trees/Program.cs
@@ -4,20 +4,22 @@
   {
     string[] input = File.ReadAllLines("D:\\Programming\\repos\\aventOfCode\\AdventOfCode\\2022\\Day8\\csharp\\trees\\input.txt");
     int[][] index = SerializeInput(input);
-    int totalVisible = 0;
+    int bestScenicScore = 0;
 
     for (int i = 0; i < index.Length; i++)
     {
       for (int j = 0; j < index[0].Length; j++)
       {
-        if (IsTreeVisible(index, i, j))
+        int tempScore = GetScenicScore(index, i, j);
+        
+        if (tempScore > bestScenicScore)
         {
-          totalVisible += 1;
+          bestScenicScore = tempScore;
         }
       }
     }
 
-    Console.WriteLine(totalVisible);
+    Console.WriteLine(bestScenicScore);
     Console.ReadLine();
   }
 
@@ -44,45 +46,65 @@
     return index;
   }
 
-  private static bool IsTreeVisible(int[][] _index, int _yindex, int _xindex)
+  private static int GetScenicScore(int[][] _index, int _yindex, int _xindex)
   {
-    bool right = true;
-    bool left = true;
-    bool up = true;
-    bool down = true;
+    int right = 0;
+    int left = 0;
+    int up = 0;
+    int down = 0;
 
     for (int i = _xindex + 1; i < _index[_xindex].Length; i++)
     {
-      if (_index[_yindex][_xindex] <= _index[_yindex][i])
+      if (_index[_yindex][_xindex] > _index[_yindex][i])
       {
-        right = false;
+        right++;
+      }
+      else if (_index[_yindex][_xindex] <= _index[_yindex][i])
+      {
+        right++;
+        break;
       }
     }
 
     for (int i = _xindex - 1; i > -1; i--)
     {
-      if (_index[_yindex][_xindex] <= _index[_yindex][i])
+      if (_index[_yindex][_xindex] > _index[_yindex][i])
       {
-        left = false;
+        left++;
+      }
+      else if (_index[_yindex][_xindex] <= _index[_yindex][i])
+      {
+        left++;
+        break;
       }
     }
 
     for (int i = _yindex - 1; i > -1; i--)
     {
-      if (_index[_yindex][_xindex] <= _index[i][_xindex])
+      if (_index[_yindex][_xindex] > _index[i][_xindex])
       {
-        up = false;
+        up++;
+      }
+      else if (_index[_yindex][_xindex] <= _index[i][_xindex])
+      {
+        up++;
+        break;
       }
     }
 
     for (int i = _yindex + 1; i < _index[_yindex].Length; i++)
     {
-      if (_index[_yindex][_xindex] <= _index[i][_xindex])
+      if (_index[_yindex][_xindex] > _index[i][_xindex])
       {
-        down = false;
+        down++;
+      }
+      else if (_index[_yindex][_xindex] <= _index[i][_xindex])
+      {
+        down++;
+        break;
       }
     }
 
-    return right || left || up || down;
+    return right * left * up * down;
   }
 }
