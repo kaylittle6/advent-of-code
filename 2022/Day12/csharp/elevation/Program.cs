fdfcf8064538afca85498160cf515138b3395bfc
Kyle Little <klittle@iteamnm.com>
2023-04-26 22:02:10 -0600

    progress day 12

--- 2022/Day12/csharp/elevation/Program.cs
+++ 2022/Day12/csharp/elevation/Program.cs
@@ -2,42 +2,38 @@
 {
   public static void Main(string[] args)
   {
-    string[] input = File.ReadAllLines("C:\\Users\\klittle\\source\\vscPractice\\AoC\\2022\\Day12\\csharp\\elevation\\input.txt");
-    char[][] indexField = ParseAndConvertInput(input);
-    Node currentNode = new Node()
-    {
-      CurrentX = indexField[20][0],
-      //CurrentY
-    };
-      
-      
-    //var = indexField[20][0];
-    //var goalNode = indexField[20][54];
+    string[] input = File.ReadAllLines("D:\\Programming\\repos\\aventOfCode\\AdventOfCode\\2022\\Day12\\csharp\\elevation\\input.txt");
+    Node[][] indexField = ParseAndConvertInput(input);
 
+    Node startingNode = new();
+    Node goalNode = new();
 
+    GetStartingNodes(indexField, startingNode, goalNode);
 
-
-
-
+    Node currentNode = startingNode;
 
 
     Console.WriteLine(indexField);
     Console.ReadLine();
   }
 
-  public static char[][] ParseAndConvertInput(string[] _input)
+  public static Node[][] ParseAndConvertInput(string[] _input)
   {
-    char[][] indexField = new char[41][];
+    Node[][] indexField = new Node[_input.Length][];
 
     for (int i = 0; i < indexField.Length; i++)
     {
       foreach (string line in _input)
       {
-        indexField[i] = new char[80];
+        indexField[i] = new Node[line.Length];
 
         for (int j = 0; j < line.Length; j++)
         {
-          indexField[i][j] = line[j];
+          indexField[i][j] = new Node()
+          {
+            LetterValue = line[j],
+            CurrentPosition = new int[] { j, i },
+          };
         }
 
         i++;
@@ -47,18 +43,46 @@
     return indexField;
   }
 
+  public static void GetStartingNodes(Node[][] _indexField, Node _startingNode, Node _goalNode)
+  {
+    for (int i = 0; i < _indexField.Length; i++)
+    {
+      for (int j = 0; j < _indexField[i].Length; j++)
+      {
+        if (_indexField[i][j].LetterValue == 'S')
+        {
+          _startingNode.CurrentPosition[0] = j;
+          _startingNode.CurrentPosition[1] = i;
+        }
+        else if (_indexField[i][j].LetterValue == 'E')
+        {
+          _goalNode.CurrentPosition[0] = j;
+          _goalNode.CurrentPosition[1] = i;
+        }
+      }
+    }
+  }
+
+  public static void CheckSurrondingNodes(Node[][] _indexField, Node _currentNode)
+  {
+
+  }
+
   public class Node
   {
-    public int CurrentX { get; set; }
-    public int CurrentY { get; set; }
+    public int LetterValue { get; set; }
+    public int[] CurrentPosition { get; set; } = new int[2];
     public int FromStartNode { get; set; }
     public int FromEndNode { get; set; }
     public int MovementCost => FromStartNode + FromEndNode;
 
+    public int GetFromStartNodeValue(Node[][] _indexField, Node _startingNode)
+    {
+      int xdiff = Math.Abs(_startingNode.CurrentPosition[0] - CurrentPosition[0]);
+      int ydiff = Math.Abs(_startingNode.CurrentPosition[1] - CurrentPosition[1]);
 
+
+    }
   }
 }
-
-
-
 
