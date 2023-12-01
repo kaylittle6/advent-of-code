27c6aad9e4d98f6e596a9e2740d58df1477264db
Kyle Little <klittle@iteamnm.com>
2023-12-01 14:40:58 -0700

    completed race, need correct count

--- 2022/Day12/csharp/elevation/Program.cs
+++ 2022/Day12/csharp/elevation/Program.cs
@@ -25,36 +25,59 @@
     }
 
     GetSurroundingNodes(MapNodes[20][0]);
+    //MapNodes[20][0].Letter = '-';
     ClosedNodes.Add(MapNodes[20][0]);
+    MapNodes[20][55].LetterElevation = 123;
 
     do
     {
       CheckAndMove();
-      RefreshMap(MapNodes);
+      //RefreshMap(MapNodes);
 
-      //Thread.Sleep(1500);
+      //Thread.Sleep(500);
 
-    } while (!ClosedNodes.Any(n => n.XPos == 55 && n.YPos == 20));
+    } while (!ClosedNodes.Any(n => n.XPos == 20 && n.YPos == 55));
 
-    foreach (var node in ClosedNodes)
-    {
-      Console.WriteLine(node.ToString());
-    }
+    var runnerNode = MapNodes[20][55];
 
-    RefreshMap(MapNodes);
+    var pathCount = RetracePath(runnerNode);
+
+    Console.WriteLine();
+    Console.WriteLine(pathCount.Count);
 
     Console.ReadLine();
     Console.ReadLine();
     
   }
 
+  // Create new List of Open Nodes and filter by FCost & HCost to find the next movement
   public static void CheckAndMove()
   {
-    var sortedOpenNodes = OpenNodes.OrderBy(n => n.FCost).ToList();
-    var lowestFCost = sortedOpenNodes[0].FCost;
-    List<Node> lowestFCostNodes = new List<Node>();
+    //var lowestHCostOpenNodes = OpenNodes.OrderBy(n => n.HCost).ToList();
+    //var lowestHCost = lowestHCostOpenNodes[0].HCost;
+    //var lowestHCostNodes = new List<Node>();
 
-    foreach (var node in sortedOpenNodes)
+    //foreach (var node in lowestHCostOpenNodes)
+    //{
+    //  if (node.HCost == lowestHCost)
+    //  {
+    //    lowestHCostNodes.Add(node);
+    //  }
+    //}
+
+    //var lowestFCostNodes = lowestHCostNodes.OrderBy(n => n.FCost).ToList();
+
+    //GetSurroundingNodes(lowestFCostNodes[0]);
+    //lowestFCostNodes[0].Letter = '-';
+    //ClosedNodes.Add(lowestFCostNodes[0]);
+    //OpenNodes.Remove(lowestFCostNodes[0]);
+
+
+    var lowestFCostOpenNodes = OpenNodes.OrderBy(n => n.FCost).ToList();
+    var lowestFCost = lowestFCostOpenNodes[0].FCost;
+    var lowestFCostNodes = new List<Node>();
+
+    foreach (var node in lowestFCostOpenNodes)
     {
       if (node.FCost == lowestFCost)
       {
@@ -62,22 +85,21 @@
       }
     }
 
-    foreach (var node in lowestFCostNodes)
-    {
-      GetSurroundingNodes(node);
-      node.Letter = '0';
-      ClosedNodes.Add(node);
-      OpenNodes.Remove(node);
-    }
+    var lowestHCostNodes = lowestFCostNodes.OrderBy(n => n.HCost).ToList();
+
+    GetSurroundingNodes(lowestHCostNodes[0]);
+    //lowestFCostNodes[0].Letter = '-';
+    ClosedNodes.Add(lowestHCostNodes[0]);
+    OpenNodes.Remove(lowestHCostNodes[0]);
   }
 
+  // Uses the current Node to find the surrounding Nodes, filters out existing and < 0 value Nodes, assigns Node parent
   public static void GetSurroundingNodes(Node node)
   {
     var newNodes = new List<Node>();
 
     if (node.Letter == 'S')
     {
-      node.Letter = 'a';
       node.LetterElevation = 97;
     }
 
@@ -98,23 +120,44 @@
       newNodes.Add(MapNodes[node.XPos][node.YPos + 1]);
     }
 
-    //var nonZeroNodes = newNodes.Where(n => n.XPos >= 0 && n.XPos <= 79 && n.YPos >= 0 && n.YPos <= 40).ToList();
     var filterNodes = newNodes
                         .Where(n => !OpenNodes
                         .Any(openNode => openNode.XPos == n.XPos && openNode.YPos == n.YPos)
                               && !ClosedNodes
                               .Any(closedNode => closedNode.XPos == n.XPos && closedNode.YPos == n.YPos)
-                              && Math.Abs(node.LetterElevation - n.LetterElevation) <= 1)
+                              && n.LetterElevation != 'a'
+                              && node.LetterElevation - n.LetterElevation > -2)
                         .ToList();
 
     foreach (var n in filterNodes)
     {
-      n.Letter = '1';
+      n.ParentNode = node;
+      // n.Letter = 'X';
     }
 
     OpenNodes.AddRange(filterNodes);
   }
 
+  // Creates a "Runner" Node to retrace the shortest path backwards
+  public static List<Node> RetracePath(Node node)
+  {
+    var pathNodes = new List<Node>();
+    var currentNode = node;
+
+    while (currentNode.XPos != 20 || currentNode.YPos != 0)
+    {
+      currentNode.Letter = '-';
+      pathNodes.Add(currentNode);
+      currentNode = currentNode.ParentNode;
+
+      RefreshMap(MapNodes);
+      Thread.Sleep(500);
+    }
+
+    return pathNodes;
+  }
+
+  // Update Node Map
   public static void RefreshMap(Node[][] mapNodes)
   {
     Console.Clear();
@@ -132,10 +175,10 @@
 
   public class Node
   {
-    private readonly int StartingXPos = 0;
-    private readonly int StartingYPos = 20;
-    private readonly int EndingXPos = 55;
-    private readonly int EndingYPos = 20;
+    private readonly int StartingXPos = 20;
+    private readonly int StartingYPos = 0;
+    private readonly int EndingXPos = 20;
+    private readonly int EndingYPos = 55;
     public int XPos { get; set; }
     public int YPos { get; set; }
     public int GCost => Math.Abs((XPos - StartingXPos) * 10) + Math.Abs((YPos - StartingYPos) * 10);
@@ -143,6 +186,7 @@
     public int FCost => GCost + HCost;
     public char Letter { get; set; }
     public int LetterElevation { get; set; }
+    public Node? ParentNode { get; set; }
 
     public Node(int xPos, int yPos)
     {
